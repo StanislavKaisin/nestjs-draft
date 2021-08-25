ce607e4826443310168d4f904ae3f8dc6d8ad662
StanislavKaisin <41913995+StanislavKaisin@users.noreply.github.com>
2021-08-26 00:11:52 +0300

    added connection to existing db and route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Movie } from './schemas/movie.schema';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  findAll(): Promise<Movie[]> {
+    return this.appService.findAll();
   }
 }
